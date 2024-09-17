bb49e9255ca422886dfbfb900315bf45861fda97
Macdonald Zharima <67922713+macdee661@users.noreply.github.com>
2024-09-16 21:31:33 -0400

    Time: 125 ms (73.06%), Space: 19.8 MB (39.79%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -1,33 +1,42 @@
-from collections import deque
+from collections import defaultdict, deque
+
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        def diff_by_one(word1, word2):
-            count = 0
-            for index in range(len(word1)):
-                if word1[index] != word2[index]:
-                    count +=1
-                    if count > 1:
-                        return False
-            return True if count == 1 else False
+        if endWord not in wordList:
+            return 0
         
-        count = 0
-        queue = deque([beginWord])
-        visited = set()
-
+        # Pre-process the word list into a dictionary of patterns
+        word_len = len(beginWord)
+        all_combo_dict = defaultdict(list)
+        
+        for word in wordList:
+            for i in range(word_len):
+                pattern = word[:i] + "*" + word[i+1:]
+                all_combo_dict[pattern].append(word)
+        
+        # BFS
+        queue = deque([(beginWord, 1)])
+        visited = set([beginWord])
+        
         while queue:
-            count += 1
-            for _ in range(len(queue)):
-                word = queue.popleft()
-                visited.add(word)
-
-                if word == endWord:
-                    return count
-
-                for new_word in wordList :
-                    if diff_by_one(new_word, word) and new_word not in visited:
-                        queue.append(new_word)
-                        visited.add(new_word)
+            current_word, level = queue.popleft()
+            
+            for i in range(word_len):
+                pattern = current_word[:i] + "*" + current_word[i+1:]
+                
+                for word in all_combo_dict[pattern]:
+                    if word == endWord:
+                        return level + 1
+                    
+                    if word not in visited:
+                        visited.add(word)
+                        queue.append((word, level + 1))
+                
+                # Clear the list to prevent redundant processing
+                all_combo_dict[pattern] = []
+        
         return 0
+
             
 
 
