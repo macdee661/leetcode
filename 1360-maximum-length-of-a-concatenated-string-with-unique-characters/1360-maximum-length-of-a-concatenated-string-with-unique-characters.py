c04faee39e9be8f33191ccf47a56dc5068c78239
Macdonald Zharima <67922713+macdee661@users.noreply.github.com>
2024-09-27 14:37:27 -0400

    Time: 727 ms (12.07%), Space: 16.6 MB (52.39%) - LeetHub

---
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/1360-maximum-length-of-a-concatenated-string-with-unique-characters.py
@@ -0,0 +1,31 @@
+class Solution:
+    def maxLength(self, arr: List[str]) -> int:
+        def unique(str1, str2):
+            letters = set()
+            for char in str1:
+                if char not in letters:
+                    letters.add(char)
+                else:
+                    return False
+            for char in str2:
+                if char not in letters:
+                    letters.add(char)
+                else:
+                    return False
+            return True
+
+        def dfs(res, arr):
+            if not arr:
+                return res
+            res2 = ''
+            if unique(res, arr[0]):
+                res2 = res + arr[0]
+
+            string1 = dfs(res, arr[1:])
+            string2 = dfs(res2, arr[1:])
+
+            if len(string1) > len(string2):
+                return string1
+            return string2
+
+        return len(dfs('', arr))
